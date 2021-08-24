74db8c373bf55ad57930d32fe44c25b01b8bade7
Murrayy <Murrayy@ZAKZRHB-CLLB13D.ballito.local>
2021-08-24 15:21:54 +0200

    Finished factors

--- src/JuneExam/Factors.java
+++ src/JuneExam/Factors.java
@@ -1,36 +1,45 @@
 package JuneExam;
 
 import javax.swing.JOptionPane;
+
 public class Factors {
+
     public static void main(String[] args) {
-        
+
         String num = JOptionPane.showInputDialog("Enter any Integer here");
         int numint = Integer.parseInt(num);
         int intTotal = 0;
         int factorCount = 0;
-        
+
         System.out.print("The factors of " + numint + " are: ");
-        for (int i = 1; i <= numint; i++){
-           
-            if (numint%i==0){
-               System.out.print(i + " ");
-               factorCount += 1;
-               
-               if(i != numint) {
-               intTotal += i;
-               }
+        for (int i = 1; i <= numint; i++) {
+
+            if (numint % i == 0) {
+                System.out.print(i + " ");
+                factorCount += 1;
+
+                if (i != numint) {
+                    intTotal += i;
+                }
             }
-                         
+
         }
         System.out.println();
         
-        if (factorCount == 2){
-        System.out.println(numint + " is a prime number.");
-        }
-        else{
-        System.out.println("");
+        System.out.println("The sum of the factors are: " + intTotal );
+        
+        if (factorCount == 2) {
+            System.out.println(numint + " is a prime number.");
+        } else if (factorCount > 2) {
+            System.out.println(numint + " is a composite number.");
+            if (intTotal == numint) {
+                System.out.println(numint + " is a perfect number.");
+
+            } else {
+                System.out.println(numint + " is NOT a perfect number.");
+            }
+
         }
 
     }
 }
-
