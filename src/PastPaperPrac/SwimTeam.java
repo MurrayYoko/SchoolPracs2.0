c667b2d74a60d3363b9b2937bb6b031f905639b5
Murrayy <Murrayy@ZAKZRHB-CLLB13D.ballito.local>
2021-10-21 15:23:32 +0200

    Java on past paper complete version.

--- src/PastPaperPrac/SwimTeam.java
+++ src/PastPaperPrac/SwimTeam.java
@@ -18,10 +18,16 @@ public class SwimTeam {
     static String teamListB;
     static String teamListReserve;
     static String nameInput;
+    static String highestName;
+    static String lowestName;
+    static String teamAName = "";
+    static String teamBName = "";
+    static String reserveName = "";
     static int countA = 0;
     static int countB = 0;
     static int countReserve = 0;
     static double avgTime = 0;
+    static double lowestAvg = avgTime;
 
     public static void main(String[] args) {
 
@@ -30,22 +36,30 @@ public class SwimTeam {
         teamListReserve = "Team Reserve \n";
 
         nameInput = JOptionPane.showInputDialog("Enter a swimmer's name here.");
-        
-       // while (()) {
-        //    ;
-            
-        //}
-       
-        oneSwim();
+
+        while (!nameInput.equalsIgnoreCase("XXX")) {
+            //saves average time
+            oneSwim();
+            if (lowestAvg == 0) {
+                lowestAvg = avgTime;
+                lowestName = nameInput;
+            }
+            if (avgTime < lowestAvg && avgTime > 0) {
+                lowestAvg = avgTime;
+                lowestName = nameInput;
+            }
+
+            nameInput = JOptionPane.showInputDialog("Enter a swimmer's name here.");
+        }
+        System.out.println("The fastest swimmer was " + lowestName + " with an average time of: " + lowestAvg + " seconds." + "\n");
+
+        displayTeamList();
     }
 
     public static void oneSwim() {
         System.out.println("Swimmer Name: " + nameInput);
 
         int totalTime = 0;
-        int teamACount = 0;
-        int teamBCount = 0;
-        int reserveTeamCount = 0;
 
         for (int i = 1; i < 5; i++) {
 
@@ -57,22 +71,32 @@ public class SwimTeam {
             System.out.println("Swimmer Time " + i + ": " + swimMin + " min " + swimSec + " sec " + " Total swim time in seconds: " + swimTime);
         }
         String team = null;
-        double avgTime = totalTime / 4;
-        
+        avgTime = totalTime / 4;
+
         if (avgTime <= 160) {
             team = teamListA;
-            teamACount++;
+            teamAName += nameInput + ", ";
+            countA++;
         }
 
         if (avgTime <= 210) {
             team = teamListB;
-            teamBCount++;
+            teamBName += nameInput + ", ";
+            countB++;
         }
 
         if (avgTime >= 210) {
             team = teamListReserve;
-            reserveTeamCount++;
+            reserveName += nameInput + ", ";
+            countReserve++;
         }
         System.out.println("Total time for all 4 times: " + totalTime + " seconds " + "\n" + "Average time for all 4 times: " + avgTime + " seconds" + "\n" + "Team: " + team);
+    }
+
+    public static void displayTeamList() {
+
+        System.out.println(teamListA + "Amount of A-team swimmers: " + countA + "\n" + "Names: " + teamAName + "\n");
+        System.out.println(teamListB + "Amount of B-team swimmers: " + countB + "\n" + "Names: " + teamBName + "\n");
+        System.out.println(teamListReserve + "Amount of Reserve-team swimmers: " + countReserve + "\n" + "Names: " + reserveName + "\n");
     }
 }
